5fada192307adbf4098483ea23459a2b84ed762e
SuperContact <supercontact@126.com>
2016-01-30 21:27:18 +0100

    Settings

---
+++ Assets/Settings.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// A class to store global variables.
+/// </summary>
+public class Settings {
+
+	/// <summary>
+	/// The limit of the cotangent value (to prevent solver error).
+	/// </summary>
+	public static double cotLimit = 10000;
+
+}
